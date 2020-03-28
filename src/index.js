7230cdf956c0743d9cb61ee042c0dbc229d871a4
ian <chiknrice@gmail.com>
2020-03-28 23:46:17 +1300

    try to hide address bar of mobile devices on load

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import { store } from 'store';
 import { ThemeProvider, AppLoader } from 'components';
 import { CssBaseline } from '@material-ui/core';
 
+/* iOS re-orientation fix */
+if (
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i)
+) {
+  /* iOS hides Safari address bar */
+  window.addEventListener('load', function() {
+    setTimeout(function() {
+      window.scrollTo(0, 1);
+    }, 1000);
+  });
+}
+
 const Root = () => {
   return (
     <Provider store={store}>
